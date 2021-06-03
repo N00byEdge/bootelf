1eed31209f910dfc6aa386b5e7e624ac94a11a87
N00byEdge <hannesbredberg@gmail.com>
2021-06-03 19:32:21 +0200

    Change addition for bitor in page tables

--- bootelf.asm
+++ bootelf.asm
@@ -92,13 +92,13 @@ stopmemmap:
   lgdt [gdtr]
 
   mov eax, 0x1000
-  mov word [eax + 0x000], page_table + 0x2000 ; Write page table root
-  mov word [eax + 0xFF8], page_table + 0x2000 ; Upper half is same
+  mov word [eax + 0x000], page_table | 0x2000 ; Write page table root
+  mov word [eax + 0xFF8], page_table | 0x2000 ; Upper half is same
   mov cr3, eax
-  mov word [0x2000], page_table + 0x3000 ; First G
-  mov word [0x2008], page_table + 0x4000 ; Second G
-  mov word [0x2FF0], page_table + 0x3000 ; -2G
-  mov word [0x2FF8], page_table + 0x4000 ; -1G
+  mov word [0x2000], page_table | 0x3000 ; First G
+  mov word [0x2008], page_table | 0x4000 ; Second G
+  mov word [0x2FF0], page_table | 0x3000 ; -2G
+  mov word [0x2FF8], page_table | 0x4000 ; -1G
 
   ; Indentity map bottom 2G
   mov di, 0x3000
